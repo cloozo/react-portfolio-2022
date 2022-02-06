4e2984b0c1d17c63ed54d86fef5aafffc43d8438
cloozo <mybenefitchoiceinc@gmail.com>
2022-02-06 00:15:57 -0500

    updading additional files

--- src/components/About /__tests__/index.test.js
+++ src/components/About /__tests__/index.test.js
@@ -1,16 +1,19 @@
+// __tests__/About.test.js
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import About from "..";
-const { asFragment } = render(<About />);
+
 afterEach(cleanup);
 
-describe("About component", () => {
-  // First Test
-  it("matches snapshot DOM node structure", () => {
+describe("About component renders", () => {
+  it("renders", () => {
     render(<About />);
   });
 
-  // Second Test
+  it("matches snapshot DOM node structure", () => {
+    const { asFragment } = render(<About />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
-expect(asFragment()).toMatchSnapshot();
