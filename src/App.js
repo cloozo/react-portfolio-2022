bdc1fca15b86c6490ac782f6d85f066b83ce483f
cloozo <mybenefitchoiceinc@gmail.com>
2022-02-08 11:23:15 -0500

    resume link added

--- src/App.js
+++ src/App.js
@@ -13,19 +13,24 @@ function App() {
     // },
     { name: "portraits", description: "Portraits of people in my life" },
     {
-      name: "Resume",
+      name: "resume",
       description: (
-        <p>
-          I am a web developer based on Miami, FL. In my spare time, I write
-          code because I enjoy it a lot. My most recent project is one that I
-          built for a start-up company. You can find me on LinkedIn , Twitter,
-          Facebook , and Instagram. I am very proficient and familiar with HTML,
-          CSS, Javascript, and other related languages. I am a very creative web
-          developer that can take any design from simple concept to an actual
-          full fledged-webpage. My skills include but are not limited to coding
-          websites and web applications by building and optimizing secure web
-          sites for evolving business need.
-        </p>
+        <div className="my-2">
+          <p>
+            I am .,,,a web developer based on Miami, FL. In my spare time, I
+            write code because I enjoy it a lot. My most recent project is one
+            that I built for a start-up company. You can find me on LinkedIn ,
+            Twitter, Facebook , and Instagram.
+          </p>
+          <p>
+            I am very proficient and familiar with HTML, CSS, Javascript, and
+            other related languages. I am a very creative web developer that can
+            take any design from simple concept to an actual full
+            fledged-webpage. My skills include but are not limited to coding
+            websites and web applications by building and optimizing secure web
+            sites for evolving business need.
+          </p>
+        </div>
       ),
     },
     // { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
