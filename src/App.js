5efa077e346f513d458ddda2efd39c5194ff2b16
cloozo <mybenefitchoiceinc@gmail.com>
2022-02-08 12:34:21 -0500

    added resume

--- src/App.js
+++ src/App.js
@@ -31,19 +31,47 @@ function App() {
       description: (
         <div className="my-2">
           <p>
-            I am .,,,a web developer based on Miami, FL. In my spare time, I
-            write code because I enjoy it a lot. My most recent project is one
-            that I built for a start-up company. You can find me on LinkedIn ,
-            Twitter, Facebook , and Instagram.
+            Jonacin Pierre-Louis Miami, FL, 33161 (786) 553 4822
+            mybenefitchoiceinc@yahoo.com{" "}
+          </p>
+          EDUCATIONAL QUALIFICATION
+          <p>
+            University of Miami Coding Bootcamp, Full Stack Web Developer
+            Florida International University, Miami, Florida August 2021- Feb
+            2022
           </p>
           <p>
-            I am very proficient and familiar with HTML, CSS, Javascript, and
-            other related languages. I am a very creative web developer that can
-            take any design from simple concept to an actual full
-            fledged-webpage. My skills include but are not limited to coding
-            websites and web applications by building and optimizing secure web
-            sites for evolving business need.
+            Master’s in Business Administration Florida International
+            University, Miami, Florida August 2009-March 2011
           </p>
+          <p>
+            Bachelor's Degree in Studio Art Graphic/Web Design Florida State
+            University, Tallahassee, Florida July 2003{" "}
+          </p>
+          PROFESSIONAL EXPERIENCE
+          <p>
+            America’s GOP 2005 - 2010 Free Lance web design • In charge of
+            creating and maintaining the website using ColdFusion, Adobe
+            Photoshop, Fireworks, and Dreamweaver, for the company and to make
+            sure that the website was running with no technical difficulties and
+            that all the codes were running flawless. • Developed graphics that
+            effectively communicate the ideas being promoted by the company’s
+            Web site.
+          </p>
+          <p>
+            Econocaribe 2003 – 2005 RPG Programmer • Tested and debugged
+            programs as well as prepared run sheets for routine programs. Part
+            of my daily responsibilities was to do coding from flow charts and
+            assisted in determining the causes of computer or program
+            malfunctions and verified program logic by preparing tests data for
+            trial runs. • Provided status reports that give a detailed
+            description of the projects and indicated time devoted to each task
+            of the project.
+          </p>
+          <p>CERTIFICATION </p>
+          Completed the ASNA “UP AND RUNNING WITH AVR for .NET” Seminar, July
+          2004 Full Stack Web Development, Miami, FL - University Of Miami
+          February 2022
         </div>
       ),
     },
